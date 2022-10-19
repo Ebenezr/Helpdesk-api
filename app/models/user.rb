afaa50ebb3af99cfff4d9ce80592d3e4dfa3d678
aliceauko <alice.auko@student.moringaschool.com>
2022-10-19 14:02:30 +0300

    Add the user validations resolve #14

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ApplicationRecord
+  
+    before_validation :downcase_email
+
+    def downcase_email
+        email.try(:downcase!)
+    end
+
+    validates :first_name, :last_name, :email, presence: true
+    #validates email
+    validates :email, presence: true, uniqueness: true
+    validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
+
+    #validates the password
+    has_secure_password
+    validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+    validates :password, confirmation: true
+    validates :password_confirmation, confirmation: true
+    
+  
+
 end
