87724c1f246542b085b0d830b8d65c3f1cda04f6
Lewis Mwendwa Kathembe <lewis.mwendwa@student.moringaschool.com>
2022-10-20 18:09:11 +0300

    Revert "16 question model"

--- app/serializers/question_serializer.rb
+++
@@ -1,6 +0,0 @@
-class QuestionSerializer < ActiveModel::Serializer
-    attributes :id, :title, :user_id, :description, :votes, :created_at, :updated_at
-
-    has_many :answers
-    belongs_to :user
-end
