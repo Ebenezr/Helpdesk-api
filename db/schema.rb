a77cee8fc7092102c3396d8ec423e7e621e2936e
aliceauko <alice.auko@student.moringaschool.com>
2022-10-20 18:11:58 +0300

    Add tag model seed data resolve #12

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2022_10_19_103859) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_10_20_134640) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.string "description"
+    t.integer "votes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
@@ -31,11 +38,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_103859) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "questions", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "title"
-    t.string "description"
-    t.integer "votes"
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,15 @@
 
+puts "Creating tags..."
+
+code = Tag.create(name:'Code')
+programming = Tag.create(name:'Programming')
+programmer = Tag.create(name:'Programmer')
+developer = Tag.create(name:'Developer')
+technology= Tag.create(name:'technology')
+ccs3 = Tag.create(name:'CCS3')
+angular = Tag.create(name:'Angular')
+
+
 
 quiz1 = Quiz.create(title: "Quiz 1", description: "This is quiz 1", user_id: 1, votes:3)
 quiz2 = Quiz.create(title: "Quiz 2", description: "This is quiz 2", user_id: 1, votes:3)
@@ -6,3 +17,4 @@ quiz3 = Quiz.create(title: "Quiz 3", description: "This is quiz 3", user_id: 1, 
 quiz4 = Quiz.create(title: "Quiz 4", description: "This is quiz 4", user_id: 1, votes:3)
 quiz5 = Quiz.create(title: "Quiz 5", description: "This is quiz 5", user_id: 1, votes:3)
 
+puts "Seeding done!"
