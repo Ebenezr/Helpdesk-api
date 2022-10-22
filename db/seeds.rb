d9d5778fe51de05684bb5948004065a5f0e247ff
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-10-22 13:08:07 +0300

    [ADD] adds and refactors the question model resolve #16

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 
-puts "Creating tags..."
-code = Tag.create(name:'Code')
-programming = Tag.create(name:'Programming')
-programmer = Tag.create(name:'Programmer')
-developer = Tag.create(name:'Developer')
-technology= Tag.create(name:'technology')
-ccs3 = Tag.create(name:'CCS3')
-angular = Tag.create(name:'Angular')
+# puts "Creating tags..."
+# code = Tag.create(name:'Code')
+# programming = Tag.create(name:'Programming')
+# programmer = Tag.create(name:'Programmer')
+# developer = Tag.create(name:'Developer')
+# technology= Tag.create(name:'technology')
+# ccs3 = Tag.create(name:'CCS3')
+# angular = Tag.create(name:'Angular')
 
-quiz1 = Quiz.create(title: "Quiz 1", description: "This is quiz 1", user_id: 1, votes:3)
-quiz2 = Quiz.create(title: "Quiz 2", description: "This is quiz 2", user_id: 1, votes:3)
-quiz3 = Quiz.create(title: "Quiz 3", description: "This is quiz 3", user_id: 1, votes:3)
-quiz4 = Quiz.create(title: "Quiz 4", description: "This is quiz 4", user_id: 1, votes:3)
-quiz5 = Quiz.create(title: "Quiz 5", description: "This is quiz 5", user_id: 1, votes:3)
+quiz1 = Question.create(title: "Question 1", description: "This is quiz 1", user_id: 1, votes:3)
+quiz2 = Question.create(title: "Question 2", description: "This is quiz 2", user_id: 1, votes:3)
+quiz3 = Question.create(title: "Question 3", description: "This is quiz 3", user_id: 1, votes:3)
+quiz4 = Question.create(title: "Question 4", description: "This is quiz 4", user_id: 1, votes:3)
+quiz5 = Question.create(title: "Question 5", description: "This is quiz 5", user_id: 1, votes:3)
 puts "Seeding done!"
 
