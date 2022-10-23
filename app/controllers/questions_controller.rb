3b972e2114d322dadfc9aa846ecaaae8c6761760
Ebenezr <ebenezarbukosia@gmail.com>
2022-10-23 20:27:08 +0300

    [REF] code refactor

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,5 @@
 class QuestionsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   before_action :set_question, only: %i[ show update destroy ]
 
     # GET /questions
@@ -55,63 +54,6 @@ class QuestionsController < ApplicationController
         render json: { error: "Question not found" }, status: :not_found
       end
 
-      def render_unprocessable_entity_response(invalid)
-        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
-      end
+    
 end
-
-
-
-  #   # GET /questions
-  #   def index
-  #     questions = Question.all
-  #     render json: questions
-  #   end
-
-  #   # SHOW /questions/:id
-  #   def show
-  #     question = Question.find(params[:id])
-  #     render json: question
-  #   end
-
-  #   # POST /questions
-  #   def create
-  #       question = Question.create(question_params)
-  #       render json: question, status: :created
-  #   end
-
-  #   # PATCH /questions/:id
-  #   def update
-  #       question = Question.find_by(id: params[:id])
-  #       question.update(question_params)
-  #       render json: question
-  #   end
-
-  #   # DELETE /questions/:id
-  #   def destroy
-  #       question = Question.find_by(id: params[:id])
-  #       question.destroy
-  #       render json: {message: "Question successfully deleted"}
-  #   end
-
-  #   private
-
-  #   def question_params
-  #       params.permit(:name, :image, :description)
-  #   end
-
-  #   private
-
-
-  # def question_params
-  #   params.permit(:title, :description, :user_id, :votes)
-  # end
-
-  # def render_not_found_response
-  #   render json: { error: "Question not found" }, status: :not_found
-  # end
-
-  # def render_unprocessable_entity_response(invalid)
-  #   render json: { errors: invalid.record.errors }, status: :unprocessable_entity
-  # end
 
