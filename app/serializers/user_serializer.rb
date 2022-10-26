9ce2f4ad5f656806eb110d69ebe9b1ded4669e61
Ebenezr <ebenezarbukosia@gmail.com>
2022-10-26 23:41:40 +0300

    [ADD] add custom endpoint to get users questions and solutions

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :first_name, :last_name
+  attributes :id, :username, :first_name, :last_name, :email
 
 
   def fullname
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
    post '/auth/login', to: 'authentication#login' 
   #  return loged in users bookmarks
    get '/mybookmarks', to: "bookmarks#mybookmarks"
+   get '/mysolutions', to: "solutions#mysolutions"
+   get '/myquestions', to: "questions#myquestions"
    #search for questions
    get '/search/:search_term', to: "questions#search"
 
