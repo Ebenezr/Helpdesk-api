0f692f783a909887694cefc1b7b1efd4a39a2536
Ebenezr <ebenezarbukosia@gmail.com>
2022-11-02 23:55:28 +0300

    [FIX] minor fixes

--- app/serializers/solution_serializer.rb
+++ app/serializers/solution_serializer.rb
@@ -2,7 +2,7 @@ class SolutionSerializer < ActiveModel::Serializer
   attributes :id, :description, :votes, :created_at, :username, :last_name, :first_name
   belongs_to :user
   belongs_to :question
-  has_many :notifications
+  has_many :solution_notifications
   # get username of soln owner
   def username
     self.object.user.username
